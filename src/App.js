bec253666924cddd0894df80ceaab2d69c47a566
Aswin Asok <aswinasokofficial@gmail.com>
2022-03-04 21:20:17 +0530

    Name Not Found 404

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import "./App.css";
 
 const App = () => {
+  const name = null;
+  const isNameShowing = true;
+
   return (
     <div className="App">
-      <h1>Lets Go and Get It</h1>
+      <h1>Lets Go and Get It, {isNameShowing ? name : "someone"} !!</h1>
+      {name ? (
+        <>
+          <h1>{name}</h1>
+        </>
+      ) : (
+        <>
+          <h1>Name Not Found 404</h1>
+        </>
+      )}
     </div>
   );
 };
