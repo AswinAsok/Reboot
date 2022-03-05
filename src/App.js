da829e032b578e18556b02a0379948c446759e37
Aswin Asok <aswinasokofficial@gmail.com>
2022-03-05 10:30:11 +0530

    Got the Data Yay!

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+//http://www.omdbapi.com/?i=tt3896198&apikey=7ba57fd4
+
+const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=7ba57fd4";
 
 const App = () => {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data);
+  };
+
+  useEffect(() => {
+    searchMovies("Avengers");
+  }, []);
   return <h1>App</h1>;
 };
 
