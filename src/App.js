1dd32303cb80213d04caaffe706a6674b1da656d
Aswin Asok <aswinasokofficial@gmail.com>
2022-03-05 10:19:07 +0530

    Only Necessary Files

--- src/App.js
+++ src/App.js
@@ -1,32 +1,7 @@
-import "./App.css";
-import { useState, useEffect } from "react";
+import React from "react";
 
 const App = () => {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    setCount(100);
-  }, []);
-
-  return (
-    <div className="App">
-      <button
-        onClick={() => {
-          setCount(count - 1);
-        }}
-      >
-        -
-      </button>
-      <h1>{count}</h1>
-      <button
-        onClick={() => {
-          setCount(count + 1);
-        }}
-      >
-        +
-      </button>
-    </div>
-  );
+  return <h1>App</h1>;
 };
 
 export default App;
